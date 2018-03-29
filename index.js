18080df9478349b957bb59e68f6279f6b306a5c7
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-03-29 21:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 var name = "jannet"
 var height = "74"
+var weight = "130"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+console.log("Weight:")
+console.log(weight)
